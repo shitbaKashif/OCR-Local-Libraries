e458c46ec371f1a8da8b2fe475977a66073dba88
Shitba Kashif <119393230+shitbaKashif@users.noreply.github.com>
2026-06-28 22:58:01 +0500

    Delete OpdVerificationApi/Services directory
    Old

--- OpdVerificationApi/Services/TokenAuthService.cs
+++
@@ -1,8 +0,0 @@
-namespace OpdVerificationApi.Services;
-
-public class TokenAuthService
-{
-    public bool IsValid(string? token)
-        => !string.IsNullOrWhiteSpace(token)
-           && token == Environment.GetEnvironmentVariable("JAZZ_API_TOKEN");
-}
